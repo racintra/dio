8d500dc1823622eddbebfb29503c5c414548c944
Renato Cintra <renatocinttra@gmail.com>
2022-11-12 11:48:54 +0000

    Arquivo IoC Servidor Apache

---
+++ devops_aws/project_2_ioc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "###Bootcamp DIO - Project 2 - IaC###"
+echo "Atualizando o servidor..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
+cd /tmp
+
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "Fim!"
+
